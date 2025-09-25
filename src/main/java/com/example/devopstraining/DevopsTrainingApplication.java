6ff96bfe3cc2a9b2b72f44160c00c360302d7533
Jens van der Wateren <jens@myparcel.nl>
2025-09-25 14:38:54 +0200

    fix violations

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsTrainingApplication {
+public final class DevopsTrainingApplication {
 
-    public static void main(String[] args) {
+    private DevopsTrainingApplication() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static void main(final String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
     }
 }
